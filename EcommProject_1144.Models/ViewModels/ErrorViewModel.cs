daeaffd15fdf1703c55269f9725418113fc5cd6e
Aman-8414 <amanrai8298@gmail.com>
2025-04-26 19:54:24 +0530

    Model Add

---
+++ EcommProject_1144.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcommProject_1144.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
